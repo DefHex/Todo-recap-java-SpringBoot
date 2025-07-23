9416219416851c5b3d708cceaf32b0ad4f9b5df7
Roottoot <syuleiman@gmail.com>
2025-07-23 10:31:28 +0200

    Initial commit

---
+++ src/main/java/org/example/todorecapjava/TodoRecapJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.todorecapjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRecapJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRecapJavaApplication.class, args);
+	}
+
+}
