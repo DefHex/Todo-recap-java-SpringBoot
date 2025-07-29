1711d84702b6a4c04a75b81ed4d3cd4dc0d72847
Roottoot <syuleiman@gmail.com>
2025-07-29 13:35:49 +0200

    Controller tests

---
+++ src/test/java/org/example/todorecapjava/controller/TodoControllerTest.java
@@ -0,0 +1,26 @@
+package org.example.todorecapjava.controller;
+
+import org.junit.jupiter.api.Test;
+
+class TodoControllerTest {
+
+    @Test
+    void getAllTodos() {
+    }
+
+    @Test
+    void getTodoById() {
+    }
+
+    @Test
+    void createTodo() {
+    }
+
+    @Test
+    void updateTodo() {
+    }
+
+    @Test
+    void deleteTodo() {
+    }
+}
